23905ad62b37e999caecd42aee6653b75a1fc303
fabs96 <fabs96@users.noreply.github.com>
2019-04-11 10:37:04 -0500

    v1.0

---
+++ js/logic.js
@@ -0,0 +1,65 @@
+$().ready(function(){
+  $("#registro").click(function(){
+    var codigo = $("#codigo").val();
+    nombre = $("#nombre").val();
+    nota=$("#nota").val();
+
+    var estudiante= {
+      codigo : codigo,
+      nombre: nombre,
+      nota : nota
+    };
+    localStorage.setItem('estudiante',JSON.strigify(estudiante));
+    listarNotas();
+    limpiarCampos();
+  });
+
+   function listarNotas(){
+     var tabla ="";
+
+    tabla +="<table border='1'>";
+    tabla +="<tr>";
+    tabla +="<th>Codigo</th>";
+    tabla +="<th>Nombre</th>";
+    tabla +="<th>Nota</th>";
+    tabla +="<th>Editar</th>";
+    tabla +="<th>Eliminar</th>"
+    tabla +="</tr>"
+
+    for (var i = 0; i < localStorage.length; i++) {
+      var clave =localStorage.key[i];
+      var estud = $.parseJSON(localStorage.getItem(clave));
+      tabla += "<tr>";
+      tabla += "<td>"+estud.codigo+"</td>";
+      tabla += "<td>"+estud.nombre+"</td>";
+      tabla += "<td>"+estud.nota+"</td>";
+      tabla += '<td><button onclick="editarNota('\'+estud.codigo+'\')">Editar</button>"</td>';
+      tabla += '<td><button onclick="eliminarNota(\''+estud.codigo+'\')">Eliminar</button>"</td>';
+    }
+    tabla += "</table>";
+    $("#muestra").html(tabla);
+   }
+   function eliminarNota(codigo){
+     localStorage.removeItem(codigo);
+     listarNotas();
+   }
+   function editarNota(codigo){
+     var est;
+     for (var i = 0; i < localStorage.length; i++) {
+      var clave =localStorage.key[i];
+      if( clave == codigo){
+        est = $.parseJSON(localStorage.getItem(clave));
+        ("#codigo").val(est.codigo);
+        ("#nombre").val(est.nombre);
+        ("#nota").val(est.nota);
+      }
+     }
+   }
+
+   function limpiarCampos(){
+     $("#codigo").val("");
+     $("#nombre").val("");
+     $("#nota").val("");
+   }
+
+});
